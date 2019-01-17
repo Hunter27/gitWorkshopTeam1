1698845b69ffacf0f8fcdd34c13ac1f1c301be90
ilzeLourens <ilourens@retrorabbit.co.za>
2019-01-17 10:54:47 +0200

    created Rhombus

---
+++ ShapeGit/ShapeGit/Rhombus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Rhombus
+{
+
+    public void Area()
+    {
+        double side, area;
+        Console.WriteLine("Enter the Length of Side : ");
+        side = Convert.ToDouble(Console.ReadLine());
+        area = side * side;
+        Console.WriteLine("\nArea of Square: " + area);
+        Console.ReadKey();
+
+    }
+
+
+}
