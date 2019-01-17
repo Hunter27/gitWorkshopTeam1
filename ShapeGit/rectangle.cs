9bb580fdbf79e6fb15095085b5f0881f70d0baba
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-01-17 10:49:45 +0200

    rectangle area calculation created

---
+++ ShapeGit/rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class rectangle
+{
+    public void Area()
+    {
+        Console.Write("Enter side 1 of recatange: ");
+        String side1 = Console.ReadLine();
+        Console.Write("Enter side 2 of recatange: ");
+        String side2 = Console.ReadLine();
+        double area = Convert.ToInt32(side1) * Convert.ToInt32(side2);
+        Console.WriteLine(area);
+    }
+}
