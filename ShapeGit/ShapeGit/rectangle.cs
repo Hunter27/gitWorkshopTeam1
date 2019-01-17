c5f5bda44d54f9ae776c9508d3c325556b647101
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-01-17 10:56:07 +0200

    working rectangle

---
+++ ShapeGit/ShapeGit/rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeGit
+{
+
+    public class rectangle
+    {
+        public void Area()
+        {
+            Console.Write("Enter side 1 of recatange: ");
+            String side1 = Console.ReadLine();
+            Console.Write("Enter side 2 of recatange: ");
+            String side2 = Console.ReadLine();
+            double area = Convert.ToInt32(side1) * Convert.ToInt32(side2);
+            Console.WriteLine(area);
+        }
+    }
+}
