2bda412eca2b7dc053fbb94b0d1c19202d2d01d6
Hunter27 <nmotsumi@retrorabbit.co.za>
2019-01-17 10:45:53 +0200

    made circle class

---
+++ ShapeGit/circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Circle
+{
+    public void Area()
+    {
+        Console.Write("Enter Radius of cricle: ");
+        String value = Console.ReadLine();
+        double area = 2 * Math.PI * Convert.ToInt32(value);
+        Console.WriteLine(area);
+
+    }
+}
