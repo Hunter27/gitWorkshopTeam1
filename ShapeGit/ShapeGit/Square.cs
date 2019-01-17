40ae7bed61f02a9559e192a7874e0f4ecf64c0e5
NombusoRR <nsibiya@retrorabbit.co.za>
2019-01-17 10:51:50 +0200

    Square Area calculation

---
+++ ShapeGit/ShapeGit/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Square
+{
+	
+public class Square
+{
+    public Square()
+    {
+        Console.Write("Enter side of square: ");
+        String length = Console.ReadLine();
+        double area = Convert.ToInt32(length) * Convert.ToInt32(length);
+        Console.WriteLine(area);
+    }
+}
+
+}
