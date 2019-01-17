de7c0e1f243176cc8ef8b2a184b9ecf54af4c8be
Hunter27 <nmotsumi@retrorabbit.co.za>
2019-01-17 10:56:55 +0200

    added circle class to right place

---
+++ ShapeGit/ShapeGit/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeGit
+{
+    class Circle
+    {
+        public void Area()
+        {
+            Console.Write("Enter Radius of cricle: ");
+            String value = Console.ReadLine();
+            double area = 2 * Math.PI * Convert.ToInt32(value);
+            Console.WriteLine(area);
+
+        }
+    }
+}
